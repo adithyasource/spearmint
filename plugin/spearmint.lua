e8c534631d39cc9840f78a13f8a7511e41018e28
adithyasource <adithyasrc@gmail.com>
2026-04-09 20:20:34 +0530

    fix: change from old pluginname

--- plugin/spearmint.lua
+++ plugin/spearmint.lua
@@ -1,7 +1,7 @@
 -- prevent double loading
-if vim.g.loaded_simplemarks then
+if vim.g.loaded_spearmint then
   return
 end
-vim.g.loaded_simplemarks = true
+vim.g.loaded_spearmint = true
 
 require("spearmint").setup()
